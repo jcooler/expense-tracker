e0d25b644444671c58ac599a2472c1a9ba64c1e7
Jon <jon.cooler@yahoo.com>
2023-09-15 17:28:35 -0400

    Additions

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -16,15 +16,16 @@ function ExpenseItem(props) {
   };
   
   return (
+    <li>
     <Card className="expense-item">
       <ExpenseDate date={props.date} day={props.day} year={props.year} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}> Change Title </button>
     </Card>
+    </li>
   );
-}
+};
 
 export default ExpenseItem;
